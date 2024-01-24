e7c81101ce3c464ed0b71a85f7c9952a9c5dca7d
zerocoolethio <dawitluelseged@gmail.com>
2024-01-24 15:31:30 +0300

    creating and initializing server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import {FaSearch} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 export default function Header() {
   return (
-    <header className='flex justify-between p-4 gap-2 bg-slate-200 max-w-16xl mx-auto shadow-md items-center'>
+    <header className='flex justify-between sm:justify-evenly p-4 gap-2 bg-slate-200 max-w-16xl mx-auto shadow-md items-center'>
         <Link to='/'>
         <div className='flex items-center space-x-2 flex-wrap'>
             <span className='font-bold text-slate-500 text-sm sm:text-xl item-center'>Eneyew</span>
