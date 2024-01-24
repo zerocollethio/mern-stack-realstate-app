f73d4bb09ebf3b2eef5437d5ccfcc4421f3aab20
zerocoolethio <dawitluelseged@gmail.com>
2024-01-24 13:37:38 +0300

    configuring react router dom and making first tamplate for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signup from './pages/Signup'
+import About from './pages/About'
+import Login from './pages/Login'
+import Home from './pages/Home'
 export default function App() {
   return (
-    <div className=''>
-      <h1 className=''>hello world</h1>
-    </div>
+    <BrowserRouter className=''>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
